059b356c4894176347b6e5c85cf4272fc45d5d21
JanLivny <48556250+JanLivny@users.noreply.github.com>
2020-10-14 20:40:03 +0400

    added wiki mk1

--- src/anton.py
+++ src/anton.py
@@ -1,6 +1,6 @@
 import discord
 import random
-
+import wikipedia
 class Anton(discord.Client):
     async def on_ready(self):
         print("ONLINE")
@@ -9,17 +9,27 @@ class Anton(discord.Client):
         '''Handles message input'''
         if message.content.lower() == "ping":
             await message.channel.send("Pong")
-
+#coinflip
         if message.content.lower() == "!coin":
         	random_int = random.randint(1,2)
         	side = "Heads!"
         	if random_int == 2:
         		side = "Tails!"
         	await message.channel.send(side)
-
+       	if "!wiki " in message.content.lower():
+       		articles = wikipedia.search(message.content.lower(), results=2)
+       		print(articles)
+       		if len(articles) != 0:
+       			try:
+       				summary = wikipedia.summary(articles[1],sentences=2)
+       			except:
+       				await message.channel.send('Sorry, there was an error getting info about "' + articles[1]+'"')
+       			await message.channel.send(summary)
+       		else:
+       			await message.channel.send("Sorry, it seems like I can't find anything about your desired subject.")
 if __name__ == "__main__":
     client = Anton()
-    token = "NzY1NjEyODAyMTg1MzYzNDc3.X4XWfA.T9AiS-MFmlNYIhn_ELHKUKj1utE"
+    token = ""
     client.run(token)
 
 
