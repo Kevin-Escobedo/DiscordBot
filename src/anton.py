17d8f4ac67a0e1c7f1bb10ede0c4a5a99b89d72b
Jan Livny <janlabananeefc@gmail.com>
2020-10-14 20:57:34 +0400

    updated req

--- src/anton.py
+++ src/anton.py
@@ -29,7 +29,7 @@ class Anton(discord.Client):
        			await message.channel.send("Sorry, it seems like I can't find anything about your desired subject.")
 if __name__ == "__main__":
     client = Anton()
-    token = "NzY1NjEyODAyMTg1MzYzNDc3.X4XWfA.-4zZcb5UjGuPhFhY1yoYFleXvHI"
+    token = ""
     client.run(token)
 
 
